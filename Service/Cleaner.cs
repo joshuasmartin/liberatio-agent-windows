f6a1c842ffd037441fc8c894a4c89905c0a8ac9b
Joshua Shane Martin <josmar52789@gmail.com>
2014-03-09 23:00:00 -0400

    Adding cleaner functions

--- Service/Cleaner.cs
+++ Service/Cleaner.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.Management;
+using System.ServiceProcess;
+using System.Diagnostics;
 
 namespace Liberatio.Agent.Service
 {
@@ -13,10 +15,39 @@ namespace Liberatio.Agent.Service
         [DllImport("Srclient.dll")]
         public static extern int SRRemoveRestorePoint(int index);
 
+        [DllImport("dnsapi.dll", EntryPoint = "DnsFlushResolverCache")]
+        private static extern UInt32 DnsFlushResolverCache();
+
         public void Start()
         {
             int SeqNum = 335;
             //int intReturn = SRRemoveRestorePoint(SeqNum);
+
+            // Create a system restore point.
+
+            // Empty Recycle Bin
+            EmptyRecycleBin();
+
+            // Delete Windows Update cache.
+            DeleteWindowsUpdateCache();
+
+            // Delete Prefetch.
+            DeleteWindowsPrefetchFiles();
+
+            // Flush DNS cache.
+            FlushDnsCache();
+
+            // Loop all users,
+
+                // Delete Recent
+
+                // Delete thumbnails
+
+                // Delete Per User archived error reports
+
+                // Delete System archived error reports
+
+                // Delete %temp%
         }
 
         private void EnumRestorePoints()
@@ -31,33 +62,6 @@ namespace Liberatio.Agent.Service
             }
 
             //MessageBox.Show(Results.ToString());
-        }
-
-        /// <summary>
-        /// Deletes the files and directories contained within the Windows
-        /// Prefetch directory. Prefetch is used by Windows to cache certain
-        /// applications that are launched often. However, it may contain
-        /// data for applications that have been uninstalled or used long ago.
-        /// </summary>
-        private void DeleteWindowsPrefetchFiles()
-        {
-            try
-            {
-                DirectoryInfo info = new DirectoryInfo(Environment.GetEnvironmentVariable("SystemRoot") + @"\Prefetch");
-
-                foreach (FileInfo file in info.GetFiles())
-                {
-                    file.Delete();
-                }
-                foreach (DirectoryInfo dir in info.GetDirectories())
-                {
-                    dir.Delete(true);
-                }
-            }
-            catch (Exception)
-            {
-
-            }
         }
 
         /// <summary>
@@ -78,6 +82,9 @@ namespace Liberatio.Agent.Service
             }
         }
 
+        /// <summary>
+        /// Delete the contents of the $Recycle.bin directory on each drive.
+        /// </summary>
         private void EmptyRecycleBin()
         {
             DriveInfo[] drives = DriveInfo.GetDrives();
@@ -101,6 +108,87 @@ namespace Liberatio.Agent.Service
                 {
 
                 }
+            }
+        }
+
+        /// <summary>
+        /// Flushes the Windows DNS cache using P/Invoke.
+        /// </summary>
+        private void FlushDnsCache()
+        {
+            UInt32 result = DnsFlushResolverCache();
+        }
+
+        /// <summary>
+        /// Deletes the files and directories contained within the Windows
+        /// Prefetch directory. Prefetch is used by Windows to cache certain
+        /// applications that are launched often. However, it may contain
+        /// data for applications that have been uninstalled or used long ago.
+        /// </summary>
+        private void DeleteWindowsPrefetchFiles()
+        {
+            try
+            {
+                string path = Environment.GetEnvironmentVariable("SystemRoot") + @"\Prefetch";
+                DirectoryInfo info = new DirectoryInfo(path);
+
+                foreach (FileInfo file in info.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in info.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            catch (Exception exception)
+            {
+                EventLog.WriteEntry("LiberatioAgent", exception.ToString(), EventLogEntryType.Error);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the files in the Windows Update download cache only after
+        /// stopping both the Windows Update and Background Intelligent
+        /// Transfer Service (BITS) services in order to prevent corruption.
+        /// </summary>
+        private void DeleteWindowsUpdateCache()
+        {
+            ServiceController serviceWindowsUpdate = new ServiceController("wuauserv");
+            ServiceController serviceBits = new ServiceController("BITS");
+
+            // Make sure Windows Update service is stopped.
+            if (serviceWindowsUpdate.Status.Equals(ServiceControllerStatus.Running) ||
+                serviceWindowsUpdate.Status.Equals(ServiceControllerStatus.StartPending))
+            {
+                serviceWindowsUpdate.Stop();
+            }
+
+            // Make sure Background Intelligent Transfer Service service is stopped.
+            if (serviceBits.Status.Equals(ServiceControllerStatus.Running) ||
+                serviceBits.Status.Equals(ServiceControllerStatus.StartPending))
+            {
+                serviceBits.Stop();
+            }
+
+            // Delete the files and directories in the Windows Update cache.
+            try
+            {
+                string path = Environment.GetEnvironmentVariable("SystemRoot") + @"\SoftwareDistribution\Download";
+                DirectoryInfo info = new DirectoryInfo(path);
+
+                foreach (FileInfo file in info.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in info.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            catch (Exception exception)
+            {
+                EventLog.WriteEntry("LiberatioAgent", exception.ToString(), EventLogEntryType.Error);
             }
         }
     }
